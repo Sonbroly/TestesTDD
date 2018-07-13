670713fd8dbf466c30727ca357de3d01a55b3e37
Brunno <bruno_owned@hotmail.com>
2018-07-12 23:35:25 -0300

    dasd

--- src/semana/primeira/CamelCase.java
+++ src/semana/primeira/CamelCase.java
@@ -1,20 +1,64 @@
 package semana.primeira;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CamelCase {
 
 	public static List<String> converterCamelCase(String original){
+		List<String> listaNova = new ArrayList();
 		char[] originalCharArray = original.toCharArray();
-		for(char charAuxiliar : originalCharArray) {
-			if (Character.isDigit(charAuxiliar))
-				if(Character.isUpperCase(charAuxiliar))
-					
+		String originalAux = "";
+
+		for(int i = 0; i <= originalCharArray.length-1; i++) {
+			if(containsSpecialCharacter(original , i)) return isInvalid(listaNova);
+
+			if (!Character.isDigit(originalCharArray[i])) {
+				if(Character.isUpperCase(originalCharArray[i]))
+					if(nextCharacterIsUpperCase(i, originalCharArray)) {
+						if (!isFirst(i)) originalAux = breakWord(listaNova, originalAux);
+						originalAux += Character.toLowerCase(originalCharArray[i]);
+						continue;
+					} else if (isFirstAndPreviousCharacterIsUpperCase(i, originalCharArray)) originalAux = breakWord(listaNova, originalAux);
+			} else {
+				if (isFirst(i)) return isInvalid(listaNova);
+				if(nextCharacterIsUpperCase(i, originalCharArray)) originalAux = breakWord(listaNova, originalAux);
+			}
+			originalAux += originalCharArray[i];
 		}
-		List<String> listaNova = Arrays.asList(original.toLowerCase());
+
+		listaNova.add(originalAux);
 		return listaNova;
+	}
+	
+	private static String breakWord(List<String> listaNova, String originalAux) {
+		listaNova.add(originalAux);
+		return "";
+	}
+
+	public static boolean isFirstAndPreviousCharacterIsUpperCase(int i, char... originalCharArray) {
+		if(!isFirst(i) && !Character.isUpperCase(originalCharArray[i-1])) return true;
+		return false;
+	}
+	
+	public static boolean nextCharacterIsUpperCase(int i, char... originalCharArray) {
+		if(i < originalCharArray.length-1 && !Character.isUpperCase(originalCharArray[i+1])) return true;
+		return false;
+	}
+	
+	public static boolean containsSpecialCharacter(String s, int i) {
+		String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,.";
+		if (specialChars.contains(s.substring(i, i+1))) return true;
+		return false;
+	}
+	
+	public static List<String> isInvalid(List<String> listaNova) {
+		listaNova.add("Inválido");
+		return listaNova;
+	}
+	
+	public static boolean isFirst(int posicao) {
+		return posicao == 0;
 	}
 
 }
--- src/semana/primeira/TesteCamelCase.java
+++ src/semana/primeira/TesteCamelCase.java
@@ -18,5 +18,17 @@ public class TesteCamelCase {
 		assertEquals(listaOriginal, CamelCase.converterCamelCase("Nome"));
 		listaOriginal =  Arrays.asList("nome", "composto");
 		assertEquals(listaOriginal, CamelCase.converterCamelCase("nomeComposto"));
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("NomeComposto"));
+		listaOriginal =  Arrays.asList("CPF");
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("CPF"));
+		listaOriginal =  Arrays.asList("numero", "CPF");
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("numeroCPF"));
+		listaOriginal =  Arrays.asList("numero", "CPF", "contribuinte");
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("numeroCPFContribuinte"));
+		listaOriginal =  Arrays.asList("recupera", "10", "primeiros");
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("recupera10Primeiros"));
+		listaOriginal =  Arrays.asList("Inválido");
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("10Primeiros"));
+		assertEquals(listaOriginal, CamelCase.converterCamelCase("nome#Composto"));
 	}
 }
